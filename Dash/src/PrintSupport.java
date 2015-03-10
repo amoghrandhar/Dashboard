332c2586d519fb330c6a0074528d83090c68e08c
marcosss3 <kcts1g13@soton.ac.uk>
2015-03-10 10:36:24 +0000

    Printing fixed?

--- Dash/src/PrintSupport.java
+++ Dash/src/PrintSupport.java
@@ -1,10 +1,13 @@
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
+
 import javax.swing.RepaintManager;
  
 /**
@@ -38,17 +41,54 @@ public class PrintSupport implements Printable {
     }
  
     @Override
-    public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
-        if (pageIndex > 0) {
-            return (NO_SUCH_PAGE);
-        } else {
-            Graphics2D g2d = (Graphics2D) g;
-            g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-            disableDoubleBuffering(print_component);
-            print_component.paint(g2d);
-            enableDoubleBuffering(print_component);
-            return (PAGE_EXISTS);
+    public int print(Graphics g, PageFormat pf, int pageNumber) {
+    	// TODO Auto-generated method stub
+        if (pageNumber > 0) {
+            return Printable.NO_SUCH_PAGE;
         }
+
+        // Get the preferred size ofthe component...
+        Dimension compSize = print_component.getPreferredSize();
+        // Make sure we size to the preferred size
+        print_component.setSize(compSize);
+        // Get the the print size
+        Dimension printSize = new Dimension();
+        printSize.setSize(pf.getImageableWidth(), pf.getImageableHeight());
+
+        // Calculate the scale factor
+        double scaleFactor = getScaleFactorToFit(compSize, printSize);
+        // Don't want to scale up, only want to scale down
+        if (scaleFactor > 1d) {
+            scaleFactor = 1d;
+        }
+
+        // Calcaulte the scaled size...
+        double scaleWidth = compSize.width * scaleFactor;
+        double scaleHeight = compSize.height * scaleFactor;
+
+        // Create a clone of the graphics context.  This allows us to manipulate
+        // the graphics context without begin worried about what effects
+        // it might have once we're finished
+        Graphics2D g2 = (Graphics2D) g.create();
+        // Calculate the x/y position of the component, this will center
+        // the result on the page if it can
+        double x = ((pf.getImageableWidth() - scaleWidth) / 2d) + pf.getImageableX();
+        double y = ((pf.getImageableHeight() - scaleHeight) / 2d) + pf.getImageableY();
+        // Create a new AffineTransformation
+        AffineTransform at = new AffineTransform();
+        // Translate the offset to out "center" of page
+        at.translate(x, y);
+        // Set the scaling
+        at.scale(scaleFactor, scaleFactor);
+        // Apply the transformation
+        g2.transform(at);
+        // Print the component
+        print_component.printAll(g2);
+        // Dispose of the graphics context, freeing up memory and discarding
+        // our changes
+        g2.dispose();
+        print_component.revalidate();
+        return Printable.PAGE_EXISTS;
     }
  
     public static void disableDoubleBuffering(Component c) {
@@ -59,5 +99,39 @@ public class PrintSupport implements Printable {
     public static void enableDoubleBuffering(Component c) {
         RepaintManager currentManager = RepaintManager.currentManager(c);
         currentManager.setDoubleBufferingEnabled(true);
+    }
+    
+    public static double getScaleFactorToFit(Dimension original, Dimension toFit) {
+
+        double dScale = 1d;
+
+        if (original != null && toFit != null) {
+
+            double dScaleWidth = getScaleFactor(original.width, toFit.width);
+            double dScaleHeight = getScaleFactor(original.height, toFit.height);
+
+            dScale = Math.min(dScaleHeight, dScaleWidth);
+
+        }
+
+        return dScale;
+
+    }
+
+    public static double getScaleFactor(int iMasterSize, int iTargetSize) {
+
+        double dScale = 1;
+        if (iMasterSize > iTargetSize) {
+
+            dScale = (double) iTargetSize / (double) iMasterSize;
+
+        } else {
+
+            dScale = (double) iTargetSize / (double) iMasterSize;
+
+        }
+
+        return dScale;
+
     }
 }
